f9a6bc0f236f37bf4fe98b1e4235950d51d2a89f
FrancaTM <francatm@outlook.com>
2020-12-10 11:09:11 -0300

    change server code to simple version

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// other server
 const db = require("./app/models");
+const controller = require("./app/controllers/tutorial.controller");
+
+const run = async () => {};
+
+// db.sequelize.sync();
 db.sequelize.sync({ force: true }).then(() => {
   console.log("drop and re-sync db");
 });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "welcome to francaTM application" });
-});
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 
-require("./app/routes/tutorial.routes")(app);
+// const app = express();
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+// var corsOptions = {
+//   origin: "http://localhost:8081",
+// };
+
+// app.use(cors(corsOptions));
+
+// // parse requests of content-type - application/json
+// app.use(bodyParser.json());
+
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const db = require("./app/models");
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("drop and re-sync db");
+// });
+
+// // simple route
+// app.get("/", (req, res) => {
+//   res.json({ message: "welcome to francaTM application" });
+// });
+
+// require("./app/routes/tutorial.routes")(app);
+
+// // set port, listen for requests
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`server is running on port ${PORT}`);
+// });
